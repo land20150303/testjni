54d5a718be34c05082df65549e6c091ceabfe0c1
land20150303 <land20150303@126.com>
2015-11-20 11:15:06 +0800

    Initial commit:testjni

---
+++ app/src/main/java/com/unit/testjni/MainActivity.java
@@ -0,0 +1,24 @@
+package com.unit.testjni;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("JniTest");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtView = (TextView) findViewById(R.id.txt);
+        txtView.setText(getStringFromNative());
+
+    }
+
+    public native String getStringFromNative();
+}
